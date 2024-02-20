20a56641400b614ceaaefd54053465ebe2d43254
tariksaid-dev <tariksaid.dev@gmail.com>
2024-02-20 20:23:40 +0100

    Fixing css

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,7 +1,7 @@
 function MainLayout({ children }) {
   return (
     // <div className="h-full bg-skin-dark-blue text-white text-base selection:bg-skin-orange-lanze selection:text-white">
-        <div className="h-full text-white text-base selection:bg-skin-orange-lanze selection:text-white bg-background orange-theme">
+        <div className="min-h-screen bg-background font-sans antialiased theme-orange dark">
       {children}
     </div>
   );
