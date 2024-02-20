a6ec1d85ebbfa7f687b6738f16b96a98bf96ae8f
tariksaid-dev <tariksaid.dev@gmail.com>
2024-02-20 21:44:55 +0100

    Comments deleted

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,6 @@ function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <MainLayout>
-        {/* <h1 className="h-[240vh] bg-skin-dark-blue text-white text-base selection:bg-skin-orange-lanze selection:text-white">
-        Hola mundo
-      </h1> */}
         <DinamicHeader></DinamicHeader>
       </MainLayout>
     </ThemeProvider>
