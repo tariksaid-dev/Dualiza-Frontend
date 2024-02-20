d7d342b814d8c6a054f6027fbeeb73f922045a80
tariksaid-dev <tariksaid.dev@gmail.com>
2024-02-20 21:00:03 +0100

    Css fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import MainLayout from "./components/layouts/MainLayout";
 import DinamicHeader from "./components/ui/Footer";
+import { ThemeProvider } from "./context/DarkModeContext";
 
 function App() {
   return (
-    <MainLayout>
-      {/* <h1 className="h-[240vh] bg-skin-dark-blue text-white text-base selection:bg-skin-orange-lanze selection:text-white">
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <MainLayout>
+        {/* <h1 className="h-[240vh] bg-skin-dark-blue text-white text-base selection:bg-skin-orange-lanze selection:text-white">
         Hola mundo
       </h1> */}
-      <DinamicHeader></DinamicHeader>
-    </MainLayout>
+        <DinamicHeader></DinamicHeader>
+      </MainLayout>
+    </ThemeProvider>
   );
 }
 
--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,7 +1,12 @@
+import {Button} from "@/components/ui/button"
+
 function MainLayout({ children }) {
   return (
     // <div className="h-full bg-skin-dark-blue text-white text-base selection:bg-skin-orange-lanze selection:text-white">
-        <div className="min-h-screen bg-background font-sans antialiased theme-orange dark">
+        <div className="min-h-screen bg-background text-primary-foreground font-sans antialiased theme-orange dark">
+          <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter ">Hello there</h1>
+          <h2 className="text-muted-foreground">Hi!</h2>
+          <Button className="bg-primary">Button</Button>
       {children}
     </div>
   );
