b14fcbca7b6f0e4ea0f66630fe5ab14063f03a8e
PepeRivera04 <peperivera1fpb@gmail.com>
2024-03-03 19:52:30 +0100

    Añadir useEffect para obtener noticias y actualizar la paginación

--- src/features/admin-panel/AdminNews.jsx
+++ src/features/admin-panel/AdminNews.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Pagination,
   PaginationContent,
@@ -9,84 +9,17 @@ import {
   PaginationPrevious,
 } from "@/components/ui/pagination";
 import { useDeleteNew } from "./useDeleteNew";
+import { useNews } from "../news/useNews";
+import Spinner from "@/components/ui/Spinner";
 
-const noticias = [
-  {
-    id: "1",
-    titulo: "Nueva actualización del sistema operativo1",
-    descripcion:
-      "Se ha lanzado una nueva actualización del sistema operativo que incluye mejoras de rendimiento y correcciones de errores.",
-    imagen: "https://example.com/imagen1.jpg",
-  },
-  {
-    id: "2",
-    titulo: "Lanzamiento de un nuevo producto2",
-    descripcion:
-      "La empresa X ha anunciado el lanzamiento de un nuevo producto revolucionario que promete cambiar la forma en que hacemos las cosas.",
-    imagen: "https://example.com/imagen2.jpg",
-  },
-  {
-    id: "3",
-    titulo: "Resultados de la investigación científica3",
-    descripcion:
-      "Un equipo de científicos ha publicado los resultados de su investigación sobre el cambio climático, destacando la necesidad de tomar medidas urgentes.",
-    imagen: "https://example.com/imagen3.jpg",
-  },
-  {
-    id: "4",
-    titulo: "Evento cultural en la ciudad4",
-    descripcion:
-      "Se llevará a cabo un evento cultural en la ciudad este fin de semana, con música en vivo, arte callejero y actividades para toda la familia.",
-    imagen: "https://example.com/imagen4.jpg",
-  },
-  {
-    id: "5",
-    titulo: "Nuevas medidas de seguridad en aeropuertos5",
-    descripcion:
-      "Los aeropuertos han implementado nuevas medidas de seguridad en respuesta a las recientes amenazas de seguridad.",
-    imagen: "https://example.com/imagen5.jpg",
-  },
-  {
-    id: "6",
-    titulo: "Descubrimiento arqueológico",
-    descripcion:
-      "Un equipo de arqueólogos ha hecho un importante descubrimiento en un sitio arqueológico antiguo, revelando secretos del pasado.",
-    imagen: "https://example.com/imagen6.jpg",
-  },
-  {
-    id: "7",
-    titulo: "Lanzamiento de una nueva película",
-    descripcion:
-      "La esperada película dirigida por el famoso director ha sido lanzada, atrayendo la atención de críticos y fanáticos por igual.",
-    imagen: "https://example.com/imagen7.jpg",
-  },
-  {
-    id: "8",
-    titulo: "Campaña de concientización sobre el reciclaje",
-    descripcion:
-      "Se ha lanzado una campaña de concientización sobre la importancia del reciclaje para proteger el medio ambiente y reducir la contaminación.",
-    imagen: "https://example.com/imagen8.jpg",
-  },
-  {
-    id: "9",
-    titulo: "Entrevista exclusiva con el autor",
-    descripcion:
-      "Una entrevista exclusiva con el autor del libro más vendido revela detalles fascinantes sobre su proceso de escritura y sus inspiraciones.",
-    imagen: "https://example.com/imagen9.jpg",
-  },
-  {
-    id: "10",
-    titulo: "Apertura de un nuevo centro de investigación",
-    descripcion:
-      "Se ha inaugurado un nuevo centro de investigación que se centrará en el desarrollo de tecnologías innovadoras para abordar los desafíos globales.",
-    imagen: "https://example.com/imagen10.jpg",
-  },
-];
 const noticiasPorPagina = 4;
 
 const AdminNews = () => {
   const [paginaActual, setPaginaActual] = useState(1);
-  const totalPaginas = Math.ceil(noticias.length / noticiasPorPagina);
+  const [noticias, setNoticias] = useState([]);
+  const { news } = useNews();
+
+  const totalPaginas = Math.ceil(news?.length / noticiasPorPagina);
 
   const { deleteNew } = useDeleteNew();
 
@@ -104,7 +37,7 @@ const AdminNews = () => {
 
   const startIndex = (paginaActual - 1) * noticiasPorPagina;
   const endIndex = startIndex + noticiasPorPagina;
-  const noticiasPaginadas = noticias.slice(startIndex, endIndex);
+  const noticiasPaginadas = news.slice(startIndex, endIndex);
 
   const handleDelete = (id) => {
     deleteNew(id);
@@ -114,6 +47,10 @@ const AdminNews = () => {
   const handleEditar = () => {
     console.log("Editar noticia");
   };
+
+  useEffect(() => {
+    setNoticias(news);
+  }, [news]);
 
   return (
     <div className="">
