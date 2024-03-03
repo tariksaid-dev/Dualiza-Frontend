9bbdb1f7356e4ad3e994fa6a2624da22c8690b42
NuMeRo-999 <pedrovp003@gmail.com>
2024-03-03 20:08:30 +0100

    News info change

--- src/features/news/NewsContainer.jsx
+++ src/features/news/NewsContainer.jsx
@@ -9,116 +9,27 @@ import CenterNews from "./CenterNews";
 
 const NewsContainer = () => {
 
-  const newsData2 = [
-    {
-      id: 1,
-      title: "Alumnos del IES ALbaytar colaboran para evaluar la eficiencias energética en centros",
-      created_at: "2022-10-01",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      image: "milei.jpg",
-    },
-    {
-      id: 2,
-      title: "Exploring the Impact of Quantum Computing on Modern Technology",
-      created_at: "2022-10-02",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      image: "milei.jpg",
-    },
-    {
-      id: 3,
-      title: "The Role of AI in Personalized Medicine: Revolutionizing Healthcare",
-      created_at: "2022-10-03",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      image: "milei.jpg",
-    },
-    {
-      id: 4,
-      title: "The Role of AI in Personalized Medicine: Revolutionizing Healthcare",
-      created_at: "2022-10-04",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      image: "milei.jpg",
-    },
-    {
-      id: 5,
-      title: "The Role of AI in Personalized Medicine: Revolutionizing Healthcare",
-      created_at: "2022-10-05",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      image: "milei.jpg",
-    },
-    {
-      id: 6,
-      title: "The Role of AI in Personalized Medicine: Revolutionizing Healthcare",
-      created_at: "2022-10-06",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      image: "milei.jpg",
-    },
-    {
-      id: 7,
-      title: "The Role of AI in Personalized Medicine: Revolutionizing Healthcare",
-      created_at: "2022-10-06",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      image: "milei.jpg",
-    },
-    {
-      id: 8,
-      title: "The Role of AI in Personalized Medicine: Revolutionizing Healthcare",
-      created_at: "2022-10-06",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      image: "milei.jpg",
-    },
-    {
-      id: 9,
-      title: "The Role of AI in Personalized Medicine: Revolutionizing Healthcare",
-      created_at: "2022-10-06",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      image: "milei.jpg",
-    },
-    {
-      id: 10,
-      title: "The Role of AI in Personalized Medicine: Revolutionizing Healthcare",
-      created_at: "2022-10-06",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      image: "milei.jpg",
-    },
-    {
-      id: 11,
-      title: "The Role of AI in Personalized Medicine: Revolutionizing Healthcare",
-      created_at: "2022-10-06",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      image: "milei.jpg",
-    },
-    {
-      id: 12,
-      title: "The Role of AI in Personalized Medicine: Revolutionizing Healthcare",
-      created_at: "2022-10-06",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      image: "milei.jpg",
-    },
-  ];
-
-  
   const { isLoading, error, news } = useNews();
-  console.log(news);
   
   if(isLoading) return <Spinner></Spinner>
-  
 
-  const sortedNews = [...newsData2].sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+
+  const sortedNews = [...news].sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
 
   const mainNew = sortedNews[0] ? [sortedNews[0]] : [];
   const sideNews = sortedNews.slice(1, 3);
   const otherNews = sortedNews.slice(3, 6);
 
-  const hlanzNews = sortedNews.filter(item => item.categoria === "HLANZ");
-  const virgenNews = sortedNews.filter(item => item.categoria === "VIRGENG");
-  const albaytarNews = sortedNews.filter(item => item.categoria === "ALBAYTAR");
-
-  sideNews.map((item) => {console.log(item);})
+  const hlanzNews = sortedNews.filter(item => item.category === "hlanz");
+  const virgenNews = sortedNews.filter(item => item.category === "virgen");
+  const albaytarNews = sortedNews.filter(item => item.category === "albaytar");
 
   return (
     
     <section className="m-auto h-full w-5/6 py-6 md:py-12 xl:py-16">
-      <h1 className="text-center text-5xl font-bold p-4 mb-10">NOTICIAS DUALIZA</h1>
+      <div className="flex justify-center">
+        <h1 className="text-center text-5xl font-bold p-4 mb-10">NOTICIAS DUALIZA</h1>
+      </div>
       <div className="container px-4 md:px-6">
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 mb-20 justify-center">
 
@@ -140,13 +51,13 @@ const NewsContainer = () => {
           ))}
         </div>
         
-        <div className="flex flex-col gap-10">
+        <div className="flex flex-col gap-12">
 
-          <CenterNews centro={'IES Hermenegildo Lanz'} news={sortedNews}/>
+          <CenterNews centro={'IES Hermenegildo Lanz'} news={hlanzNews}/>
 
-          <CenterNews centro={'IES Virgen de Gracia'} news={sortedNews}/>
+          <CenterNews centro={'IES Virgen de Gracia'} news={virgenNews}/>
 
-          <CenterNews centro={'IES Albaytar'} news={sortedNews}/>
+          <CenterNews centro={'IES Albaytar'} news={albaytarNews}/>
 
         </div>
       </div>
