d90385131decf8737f4765f9b92192285b9ecf0e
NuMeRo-999 <pedrovp003@gmail.com>
2024-03-05 22:49:04 +0100

    login changes, login icon changes

--- src/features/login-form/LoginForm.jsx
+++ src/features/login-form/LoginForm.jsx
@@ -4,6 +4,7 @@ import { Input } from '@/components/ui/input';
 import BombillaLibro from '@/components/icons/BombillaLibro';
 import StaticHeader from '@/components/ui/StaticHeader';
 import LetrasBombilla from '@/components/icons/LetrasBombilla';
+import LogoCompleto from '@/components/icons/LogoCompleto';
 
 
 const LoginForm = () => {
@@ -42,25 +43,16 @@ const LoginForm = () => {
 
         <div className=" rounded-[0.5rem] border border-border bg-background shadow-md md:shadow-xl">
 
-          <div className="container relative  h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+          <div className="container relative h-[80vh] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div className="relative hidden h-full flex-col sm:p-10 text-white lg:flex dark:border-r">
               <div className="absolute inset-0 bg-zinc-900">
-              </div>
-              <div className="relative z-20 flex items-center text-lg font-medium text-card-foreground">
-                <BombillaLibro className="w-8 h-8 mr-2" fill='#fff' />
-
               </div>
               <div className="relative z-20 mt-auto">
                 <div className='pb-24'>
 
-                  <BombillaLibro height='400' width='400' />
+                  <LogoCompleto width='400' height='400'/>
                 </div>
-                <blockquote className="space-y-2">
-                  <div className='flex items-center justify-center'>
-
-                    <LetrasBombilla height='100' width='300' fill='#fff' />
-                  </div>
-                </blockquote>
+                
               </div>
             </div>
 
@@ -97,16 +89,6 @@ const LoginForm = () => {
                   </form>
 
                 </div>
-                <p className="px-8 text-center text-sm text-muted-foreground">
-                  By clicking continue, you agree to our
-                  <a className="underline underline-offset-4 hover:text-primary" href="/terms"
-                  >Terms of Service
-                  </a>
-                  and
-                  <a className="underline underline-offset-4 hover:text-primary" href="/privacy">
-                    Privacy Policy
-                  </a>.
-                </p>
               </div>
             </div>
           </div>
