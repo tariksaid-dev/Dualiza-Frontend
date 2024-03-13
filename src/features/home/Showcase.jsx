372bbb132c813e9fd8b1898b9f1783cfcd855adc
RogerCiv <rogeliocivantos@gmail.com>
2024-03-13 18:15:57 +0100

    fix news again

--- src/features/home/Showcase.jsx
+++ src/features/home/Showcase.jsx
@@ -41,7 +41,7 @@ const Showcase = () => {
           {isLoading && <Spinner />}
           {news &&
             news
-              .filter((_, index) => (isMobile ? index < 3 : index < 9))
+              .filter((_, index) => (isMobile ? index < 3 : index < 6))
               .map((n) => <ShowCaseCard key={n.id} site={n} />)}
         </div>
         <div className="flex flex-1 justify-end">
