fd6ffb2ddfbd84f025c4aa839ea344a65d217d8e
tariksaid-dev <tariksaid.dev@gmail.com>
2024-03-17 00:35:03 +0100

    Fix spinner component

--- src/components/ui/Spinner.jsx
+++ src/components/ui/Spinner.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-
-const Spinner = () => {
-	return (
-		<div className="animate-spin inline-block size-6 border-[3px] border-current border-t-transparent text-blue-900 rounded-full" role="status" aria-label="loading">
-			<span className="sr-only">Loading News...</span>
-		</div>
-	)
+function Spinner({ size = "144px", width = "7px" }) {
+  return (
+    <div
+      className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center"
+      role="status"
+      aria-label="loading"
+    >
+      <div
+        className={`animate-spin inline-block size-[${size}] border-[${width}] border-current border-t-transparent text-primary rounded-full`}
+      ></div>
+      <span className="sr-only">Loading News...</span>
+    </div>
+  );
 }
 
-export default Spinner
+export default Spinner;
