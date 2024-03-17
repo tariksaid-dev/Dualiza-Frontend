0b2fe1eb9ebe1b920f4fd88212589f3a27d09891
tariksaid-dev <tariksaid.dev@gmail.com>
2024-03-17 06:07:40 +0100

    Fix imports

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -2,11 +2,11 @@ import React, { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 
 import AdminPanelLayout from "@/components/layouts/AdminPanelLayout";
-import AdminPanelHeader from "@/features/admin-panel/AdminPanelHeader";
-import AdminPanelCard from "@/features/admin-panel/AdminPanelCard";
-import AdminNews from "@/features/admin-panel/AdminNews";
-import AdminEmails from "@/features/admin-panel/AdminEmails";
-import { AdminUsers } from "@/features/admin-panel/AdminUsers";
+import AdminPanelHeader from "@/features/admin-panel/header/AdminPanelHeader";
+import AdminPanelCard from "@/features/admin-panel/body-card/AdminPanelCard";
+import AdminNews from "@/features/admin-panel/body-card/news/AdminNews";
+import AdminEmails from "@/features/admin-panel/body-card/emails/AdminEmails";
+import AdminUsers from "@/features/admin-panel/body-card/users/AdminUsers";
 
 function Admin() {
   const [searchParams, setSearchParams] = useSearchParams();
