bfa912f09f3858ce23fff45530150c376729ab1c
tariksaid-dev <tariksaid.dev@gmail.com>
2024-03-30 00:16:13 +0100

    Add admin news create component and delete old create/edit new

---
+++ src/features/admin-panel/body-card/news/AdminNewsCreate.jsx
@@ -0,0 +1,12 @@
+import AdminPanelCardBody from "../AdminPanelCardBody";
+import CreateNewForm from "./CreateNewForm";
+
+function AdminNewsCreate() {
+  return (
+    <AdminPanelCardBody>
+      <CreateNewForm />
+    </AdminPanelCardBody>
+  );
+}
+
+export default AdminNewsCreate;
