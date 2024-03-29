79e6e3f03a942d98edec6db55eee760fc1260866
tariksaid-dev <tariksaid.dev@gmail.com>
2024-03-30 00:10:32 +0100

    Update routes deleteing old ones

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-import Home from "./pages/Home";
 import ProtectedRoute from "./utils/ProtectedRoute";
+import Home from "./pages/Home";
 import Admin from "./pages/Admin";
 import Login from "./pages/Login";
 import News from "./pages/News";
-import ErrorPage from "./pages/ErrorPage";
 import NewsDetails from "./pages/NewsDetails";
+import ErrorPage from "./pages/ErrorPage";
 
 import { ThemeProvider } from "@/context/DarkModeContext";
 import { Toaster } from "./components/ui/sonner";
-import CreateNew from "./features/admin-panel/body-card/news/CreateNew";
-import EditNew from "./features/admin-panel/body-card/news/EditNew";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -38,16 +36,7 @@ function App() {
                 </ProtectedRoute>
               }
             ></Route>
-            <Route
-              path="admin/createNew"
-              element={
-                <ProtectedRoute redirectPath="login">
-                  <CreateNew />
-                </ProtectedRoute>
-              }
-            ></Route>
             <Route index element={<Home />} />
-            <Route path="admin/edit/:NewId" element={<EditNew />} />
             <Route path="news" element={<News />} />
             <Route path="news/:NewId" element={<NewsDetails />} />
             <Route path="login" element={<Login />} />
