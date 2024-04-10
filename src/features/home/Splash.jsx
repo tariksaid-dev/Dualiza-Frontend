3252040b723dca2f385d31ac78cf882a9756fc28
tariksaid-dev <tariksaid.dev@gmail.com>
2024-04-10 21:10:59 +0200

    Add redirect to login at splash component

--- src/features/home/Splash.jsx
+++ src/features/home/Splash.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 import "@fontsource/inter/700.css";
 import "@fontsource/inter/500.css";
-
+import { useNavigate } from "react-router-dom";
 import Bombilla from "@/components/ui/3d/bombilla/Bombilla";
 import { Button } from "@/components/ui/button";
 
 function Splash() {
+  const navigate = useNavigate();
+
+
   return (
     <section className="relative h-screen w-full mx-auto gradient-bg grid grid-cols-[60%,40%] grid-rows-1">
       <aside className="flex flex-col justify-center pl-52">
@@ -26,7 +29,7 @@ function Splash() {
         </p>
 
         <div className="flex justify-center gap-8">
-          <Button variant="default">Noticias</Button>
+          <Button variant="default" onClick={() => navigate("/login")}>Login</Button>
           <Button variant="outline">Noticias</Button>
         </div>
       </aside>
