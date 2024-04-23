3d5914addec97ae59c6df2bc829ce777bb1c70e3
tariksaid-dev <tariksaid.dev@gmail.com>
2024-04-23 04:46:17 +0200

    Add Dockerfile and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +10,14 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  preview: {
+    port: 8080,
+    strictPort: false,
+  },
+  server: {
+    port: 8080,
+    strictPort: false,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
+});
